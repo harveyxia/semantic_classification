af998cc5a25fab7622b533eb9d6ecd346d490b39
Harvey Xia <harvey@harveyxia.com>
2016-04-29 19:36:33 -0400

    Add get_cluster_members function

--- cluster.py
+++ cluster.py
@@ -5,13 +5,23 @@ import itertools
 
 import pdb 
 
-# given a dict of noun:count, generate 2-dimensional similarity matrix
-def gen_sim_matrix(noun_dict):
+def get_synset_list(noun_dict):
+    synset_list = []
     # create ordered list of nouns
     noun_list = [noun for noun in noun_dict]
-    # grab most common synset for each noun
-    # synset_list defines the mapping from index to synset for matrix
-    synset_list = [get_synset(noun) for noun in noun_list]
+    for noun in noun_list:
+        synsets = wn.synsets(noun)
+        # if no synset found
+        if len(synsets) == 0:
+            synset_list.append(None)
+        else:
+            # grab most common synset for each noun
+            synset_list.append(synsets[0])
+    return synset_list
+
+# given a dict of noun:count, generate 2-dimensional similarity matrix
+# synset_list defines the mapping from index to synset for matrix
+def gen_sim_matrix(synset_list):
     # get similarity for each pair of nouns
     sim_values = get_sim_values(synset_list)
 
@@ -30,13 +40,6 @@ def gen_sim_matrix(noun_dict):
                     matrix[i1][i2] = sim_values[(syn2, syn1)]
     return matrix
 
-def get_synset(noun):
-    synsets = wn.synsets(noun)
-    if len(synsets) == 0:
-        return None
-    else:
-        return synsets[0]
-
 def get_sim_values(synset_list):
     sim_values = defaultdict(int)
     for pair in itertools.combinations(synset_list, 2):
@@ -50,5 +53,38 @@ def get_sim_values(synset_list):
                 sim_values[pair] = 0
     return dict(sim_values)
 
+# perform hiearchical clustering
 def cluster(matrix):
     return fastcluster.linkage(matrix)
+
+# return a set of disjoint and possibly incomplete set of clusters of synsets
+# where the distance of the cluster is <= dist
+# dist is a value between 0 and 1, 1 being the max distance in the set of synsets
+def get_clusters(dist, clustering, synset_list):
+    d = dist*(clustering[-1][2])
+    clusters = []
+    # get all clusters with less than d distance
+    for cluster in clustering:
+        if cluster[2] <= d:
+            clusters.append(cluster)
+    # sort by largest clusters first
+    clusters = sorted(clusters, key=lambda x: x[3], reverse=True)
+    # assign elements to clusters uniquely
+    # for cluster in clusters:
+
+    return clusters
+
+def get_cluster_members(i, clustering, synset_list):
+    l = int(clustering[i-len(synset_list)][0])
+    r = int(clustering[i-len(synset_list)][1])
+    members = []
+    if r < len(synset_list):
+        members.append(synset_list[r])
+    else:
+        members.extend(get_cluster_members(r, clustering, synset_list))
+
+    if l < len(synset_list):
+        members.append(synset_list[l])
+    else:
+        members.extend(get_cluster_members(l, clustering, synset_list))
+    return members
--- semantic_classifier.py
+++ semantic_classifier.py
@@ -1,7 +1,19 @@
 import noun_extractor
 import cluster
 
+import pdb
+
 def run(filename):
     noun_dict = noun_extractor.get_nouns(filename)
-    matrix = cluster.gen_sim_matrix(noun_dict)
-    return matrix
+    synset_list = cluster.get_synset_list(noun_dict)
+    matrix = cluster.gen_sim_matrix(synset_list)
+    clustering = cluster.cluster(matrix)
+    # clusters = cluster.get_clusters(0.5, clustering, synset_list)
+    # pdb.set_trace()
+    return clustering
+
+filename = 'test_article.txt'
+noun_dict = noun_extractor.get_nouns(filename)
+synset_list = cluster.get_synset_list(noun_dict)
+matrix = cluster.gen_sim_matrix(synset_list)
+clustering = cluster.cluster(matrix)
